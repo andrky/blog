a15033309abd7f5e378db80bbbcce8f5e24ecaba
M. Ananda Rizky Audriansyah <anandarizky166@gmail.com>
2022-11-28 15:53:26 +0700

    Auth

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -12,5 +15,22 @@ class RegisterController extends Controller
 				'active' => 'register',
 				'gambar' => "Valdo"
 			]);
+		}
+
+		public function store(Request $request) {
+			$validateData = $request->validate([
+				'name' => 'required|max:255',
+				'username' => 'required|min:3|max:255|unique:users',
+				'email' => 'required|email:dns|unique:users',
+				'password' => 'required|min:8|max:155'
+			]);
+
+			$validateData['password'] = Hash::make($validateData['password']);
+
+			User::create($validateData);
+
+			// $request->session()->flash('success', 'Registraion success! Please login!');
+
+			return redirect('/login')->with('success', 'Registration success! Please login!');
 		}
 }
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\AboutController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -54,4 +55,10 @@ Route::get('/authors/{user:username}', function (User $user) {
 
 Route::get('/login', [LoginController::class, 'index']);
 
+Route::post('/login', [LoginController::class, 'authenticate']);
+
 Route::get('/register', [RegisterController::class, 'index']);
+
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
