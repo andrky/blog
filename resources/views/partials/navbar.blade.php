e16f660cf40947da9492dfc44114027ff780e2f9
M. Ananda Rizky Audriansyah <anandarizky166@gmail.com>
2022-11-29 15:14:38 +0700

    Middleware

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -23,11 +23,34 @@
                     <a class="nav-link {{ $active === 'category' ? 'active' : '' }}" href="/category">Category</a>
                 </li>
             </ul>
-						<ul class="navbar-nav ms-auto">
-							<li class="nav-item">
-								<a class="nav-link {{ $active === 'login' ? 'active' : '' }}" href="/login"><i class="bi bi-box-arrow-in-right pe-2"></i>Login</a>
-							</li>
-						</ul>
+
+            <ul class="navbar-nav ms-auto">
+                @auth
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Welcome, {{ auth()->user()->name }}
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" style="font-size: 13px" href="/dashboard"><i class="bi bi-house-check pe-2"></i>My Dashboard</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li>
+															<form action="/logout" method="post">
+																@csrf
+																<button class="dropdown-item" style="font-size: 13px"><i class="bi bi-box-arrow-right pe-2"></i>Logout</button>
+															</form>
+														</li>
+                        </ul>
+                    </li>
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link {{ $active === 'login' ? 'active' : '' }}" href="/login"><i
+                                class="bi bi-box-arrow-in-right pe-2"></i>Login</a>
+                    </li>
+                @endauth
+            </ul>
         </div>
     </div>
 </nav>
