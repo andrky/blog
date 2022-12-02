63b12e99483663b5b4ce6d96b96fea56403be0d1
M. Ananda Rizky Audriansyah <anandarizky166@gmail.com>
2022-12-02 16:19:36 +0700

    UI

---
+++ resources/views/dashboard/posts/show.blade.php
@@ -0,0 +1,36 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+    <div class="container">
+        <div class="row py-3">
+            <div class="col-lg-8 col-md-8 ">
+                <h1 class="mb-4">
+                    {{ $post->tittle }}
+                </h1>
+
+                <a href="/dashboard/posts" class="text-decoration-none btn btn-success"><span data-feather="arrow-left"></span></span>Back to all My Post</a>
+                <a href="#" class="text-decoration-none btn btn-warning"><span data-feather="edit"></span>Edit</a>
+                <a href="#" class="text-decoration-none btn btn-danger"><span data-feather="x-circle"></span>Delete</a>
+
+
+                <p class="fs-6 mt-3">By :
+                    <a href="/post?author={{ $post->user->username }}" class="text-decoration-none">
+                        {{ $post->user->name }}
+                    </a> in
+                    <a href="/post?category={{ $post->category->slug }}" class="text-decoration-none">
+                        {{ $post->category->name }}
+                    </a>
+                </p>
+
+                <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="img-fluid"
+                    alt="https://source.unsplash.com/500x400?{{ $post->category->name }}">
+
+                <article class="my-3">
+                    {!! $post->body !!}
+                </article>
+
+                <br />
+            </div>
+        </div>
+    </div>
+@endsection
